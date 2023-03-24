2e435f81e2767ab3e9eaf7fa462205a6d409e1b3
GeralmyUrena <geralmyuri11@gmail.com>
2023-03-23 20:11:47 -0400

    passed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React,{useState,useEffect} from "react"
+
+
+function App (){
+    const [apiData,setData] = useState()
+
+     useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(data => { setData(data.message)})
+     },[])
+
+     if(!apiData){
+        return <h2>Loading...</h2>
+     }
+     return <img alt = "A Random Dog" src={apiData}/>
+
+    //  {apiData.map((image) => image)
+
+   
+   
+}
+
+export default App
